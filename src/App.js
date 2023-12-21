61fa7054b070905170229d490fe4a822d5f0d24f
sukhii-singh <sukhi8759@gmail.com>
2023-12-21 15:52:20 +0530

    basic push

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/common/Header';
+import Header from './components/common/header/Header';
 
 function App() {
   return (
