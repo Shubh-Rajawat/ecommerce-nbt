b5e325a14d7e8d7892e02ba9471b21046dc4ae54
sukhii-singh <sukhi8759@gmail.com>
2023-12-25 11:22:16 +0530

    react-icons depndency , single product responsiveness changes

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -6,15 +6,18 @@ import p5 from "../static/assets/about1.svg";
 import p6 from "../static/assets/p6.svg";
 import p1 from "../static/assets/p1.jpg";
 import arrow from "../static/assets/arrow_r.svg";
+import { useNavigate } from "react-router-dom";
+
 const data = [
   { name: "Luger Burger", image: p5 },
   { name: "Luger Burger", image: p5 },
-  { name: "Luger Burger", image: p5 },
+  { name: "Luger Burger sdsd dsf sdf  dsfgsdsd sdf", image: p5 },
   { name: "Luger Burger", image: p5 },
   { name: "Luger Burger", image: p5 },
   { name: "Luger Burger", image: p5 },
 ];
 const Products = () => {
+  const navigate = useNavigate();
   return (
     <div className=" mt-24">
       <div className="ss-container ">
@@ -24,13 +27,16 @@ const Products = () => {
               <div
                 className=" rounded-2xl p-4 shadow-1  duration-300 cursor-pointer"
                 key={index}
+                onClick={() => {
+                  navigate("/info");
+                }}
               >
                 <div className=" rounded-2xl  overflow-hidden">
                   <img src={item?.image} alt="" />
                 </div>
                 <div className="flex mt-3 justify-between gap-3">
-                  <div className="">
-                    <h2 className="ss-head-one">Luger Burgers</h2>
+                  <div className=" overflow-hidden">
+                    <h2 className="ss-head-one truncate">{item.name}</h2>
                   </div>
                   <div className=" w-10  flex align-middle">
                     <img src={arrow} alt="arrow" className="w-2" />
