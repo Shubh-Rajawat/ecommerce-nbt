d1e838b22a629a28b5c48ea527f61b92d82d2320
sukhii-singh <sukhi8759@gmail.com>
2023-12-30 15:12:14 +0530

    cart badge in navbar

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -12,28 +12,32 @@ import LoginSingup from "../user/LoginSingup";
 
 const Header = () => {
   const dispatch = useDispatch();
-  const [ dropDown, setDropDown ] = useState( false );
+  const [dropDown, setDropDown] = useState(false);
   const navigate = useNavigate();
   return (
     <>
       <header className=" relative ">
-        {/* upper header */ }
+        {/* upper header */}
         <div className="grid grid-cols-3 ss-container">
           <div className=""></div>
           <div className=" w-[42%] bg-[#D63348] h-[45px] absolute top-0 right-0 -z-10"></div>
           <div className="h-[45px] flex justify-between col-span-3 md:col-span-2 bg-[#D63348] pl-3 md:pl-10 py-2 rounded-bl-[45px] text-white">
             <div className="font-semibold text-xs md:text-md  flex items-center gap-2">
-              { " " }
               <FaLocationDot className="text-xl" /> 5331 Rexford Court
               ,Montgomery AL 36116
             </div>
-            <div className=" text-2xl md:text-3xl flex gap-2 font-semibold text-end relative">
-              <CiShoppingCart
-                className="cursor-pointer "
-                onClick={ () => {
-                  navigate( "/cart" );
-                } }
-              />
+            <div className=" text-2xl md:text-3xl flex gap-4 font-semibold text-end relative">
+              <div
+                className="relative cursor-pointer"
+                onClick={() => {
+                  navigate("/cart");
+                }}
+              >
+                <CiShoppingCart className=" " />
+                <span className="absolute text-sm -top-2 -right-3 rounded-full text-red-600 font-semibold w-6 h-6  bg-yellow-400 p-1 flex justify-center items-center">
+                  99
+                </span>
+              </div>
               {/* <CiUser
                 className="cursor-pointer "
                 onClick={ () => {
@@ -41,51 +45,62 @@ const Header = () => {
                 } }
               />
               <LoginSingup /> */}
-              {/* Shubh code for dropdown */ }
-              <button className="cursor-pointer outline-none bg-transparent"
-                onBlur={ () => {
-                  setDropDown( false );
-                } }
-                onClick={ () => {
-                  setDropDown( !dropDown );
-                } }
+              {/* Shubh code for dropdown */}
+              <button
+                className="cursor-pointer outline-none bg-transparent"
+                onBlur={() => {
+                  setDropDown(false);
+                }}
+                onClick={() => {
+                  setDropDown(!dropDown);
+                }}
               >
-                <img src="https://bit.ly/dan-abramov" alt="User" className="h-8  rounded-full border-2 border-white" />
+                <img
+                  src="https://bit.ly/dan-abramov"
+                  alt="User"
+                  className="h-8  rounded-full border-2 border-white"
+                />
 
-                { dropDown ?
+                {dropDown ? (
                   <div class="popins z-10 bg-[#F2F2F2]  rounded-xl shadow w-48 absolute top-11 right-[5%] rounded-tr-none">
-                    <ul class="py-2 text-[16px] text-slate-800 font-[600] text-left" >
+                    <ul class="py-2 text-[16px] text-slate-800 font-[600] text-left">
                       <li>
-                        <span onClick={ () => {
-                          navigate( '/profile' )
-                        } } class=" px-4  hover:bg-gray-300 flex items-baseline gap-2"><FaUser className="text-[#D63348] text-sm" />View Profile</span>
+                        <span
+                          onClick={() => {
+                            navigate("/profile");
+                          }}
+                          class=" px-4  hover:bg-gray-300 flex items-baseline gap-2"
+                        >
+                          <FaUser className="text-[#D63348] text-sm" />
+                          View Profile
+                        </span>
                       </li>
                       <li>
-                        <span class=" px-4  hover:bg-gray-300 flex items-center gap-2"><ImExit className="text-[#D63348] text-sm" /> Logout</span>
+                        <span class=" px-4  hover:bg-gray-300 flex items-center gap-2">
+                          <ImExit className="text-[#D63348] text-sm" /> Logout
+                        </span>
                       </li>
-
                     </ul>
                   </div>
-                  : null }
+                ) : null}
               </button>
             </div>
           </div>
         </div>
-        {/* lower header */ }
-        {/* <div className="flex justify-between items-center mx-10 md:mx-20 py-6"> */ }
+        {/* lower header */}
+        {/* <div className="flex justify-between items-center mx-10 md:mx-20 py-6"> */}
         <div className="ss-container flex justify-between items-center py-6">
           <div className="w-48 md:w-64">
             <NavLink to="/">
-              { " " }
-              <img src={ logo } alt="not-found" />
+              <img src={logo} alt="not-found" />
             </NavLink>
           </div>
           <div className="">
             <RiMenu3Fill
               className="text-4xl font-extrabold cursor-pointer"
-              onClick={ () => {
-                dispatch( navAction.toggleNav() );
-              } }
+              onClick={() => {
+                dispatch(navAction.toggleNav());
+              }}
             />
           </div>
         </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,97 +1,101 @@
-import React from 'react'
-import BreadCrumb from '../components/common/BreadCrumb'
-import { FaClipboardList, FaEdit, FaPhoneAlt } from 'react-icons/fa'
+import React from "react";
+import BreadCrumb from "../components/common/BreadCrumb";
+import { FaClipboardList, FaEdit, FaPhoneAlt } from "react-icons/fa";
 import { MdOutlineLanguage } from "react-icons/md";
 import { TbHelpSquareRoundedFilled } from "react-icons/tb";
 import { IoMdSettings } from "react-icons/io";
-import { ImLocation } from 'react-icons/im'
+import { ImLocation } from "react-icons/im";
 
-import SmallCard from '../components/profile/SmallCard'
+import SmallCard from "../components/profile/SmallCard";
 
 const data = [
-    {
-        name: "Orders",
-        icon: <FaClipboardList className='text-[#D63348] text-3xl' />,
-        path: ""
-    },
-    {
-        name: "Language",
-        icon: <MdOutlineLanguage className='text-[#D63348] text-3xl' />,
-        path: ""
-    },
-    {
-        name: "Help & Support",
-        icon: <TbHelpSquareRoundedFilled className='text-[#D63348] text-3xl' />,
-        path: ""
-    },
-    {
-        name: "Settings",
-        icon: <IoMdSettings className='text-[#D63348] text-3xl' />,
-        path: ""
-    },
-]
+  {
+    name: "Orders",
+    icon: <FaClipboardList className="text-[#D63348] text-3xl" />,
+    path: "",
+  },
+  {
+    name: "Language",
+    icon: <MdOutlineLanguage className="text-[#D63348] text-3xl" />,
+    path: "",
+  },
+  {
+    name: "Help & Support",
+    icon: <TbHelpSquareRoundedFilled className="text-[#D63348] text-3xl" />,
+    path: "",
+  },
+  {
+    name: "Settings",
+    icon: <IoMdSettings className="text-[#D63348] text-3xl" />,
+    path: "",
+  },
+];
 const Profile = () => {
-    return (
-        <>
-            <BreadCrumb page={ {
-                title: "Profile",
-                path: "Home / Profile",
-                bg: "bg-[#F2F2F2]"
-            } } />
-            <div className='w-[100%] md:ss-container px-1 py-5 md:py-12 flex justify-center'>
-                <div className="w-full md:w-[60%] lg:w-[55%] xl:w-[44%] h-full bg-[#F2F2F2] px-5  md:px-7 pt-10 rounded-3xl">
-                    <div
-                        className={ `border  rounded-[20px] p-8 bg-white shadow-lg relative overflow-hidden` }>
-                        <div className=" absolute top-0 right-0 bg-[#D63348] p-4 rounded-es-xl cursor-pointer">
-                            <FaEdit color="white" size={ 22 } className="" />
-                        </div>
-                        <div className="">
-                            <span className="">
-                                <img src={ `https://bit.ly/dan-abramov` } alt="" className="h-28 md:h-40 rounded-full "
-                                    style={ {
-                                        border: '4px solid #D63348',
-                                    } }
-                                />
-                            </span>
-                            <div className="mt-3">
-                                <h3 className="popins font-semibold text-[20px]  md:text-[24px]">
-                                    John Doe
-                                </h3>
-                                <div className=" my-2 popins text-16px md:text-[18px] text-[#777777]">
-                                    examplejohn@gmail.com
-                                </div>
-                                <div className=" flex items-center gap-4 my-6 popins text-[16px] md:text-[18px]">
-                                    <div className="bg-[#D63348] text-white rounded-full  ">
-                                        <FaPhoneAlt className="  m-2" size={ 22 } />
-                                    </div>
-                                    <span className="text-[#777777]">+234 9011039271</span>
-                                </div>
-                                <div className=" flex items-center gap-4 popins text-[16px] md:text-[18px] my-2 ">
-                                    <div className="bg-[#D63348] text-white rounded-full">
-                                        <ImLocation className="text-[18px] md:text-[20px] m-2" size={ 22 } />
-                                    </div>
-                                    <span className="text-[#777777]">
-                                        No 15 Test Street London, UK, SE1 1AB
-                                    </span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {
-                        data.map( item => {
-                            return (
-                                <SmallCard key={ item.name } data={ item } />
-                            )
-                        } )
-                    }
-                    <div className="text-center mb-8">
-                        <button className="btn-red ">logout</button>
-                    </div>
-                </div>
-
+  return (
+    <>
+      <BreadCrumb
+        page={{
+          title: "Profile",
+          path: "Home / Profile",
+          bg: "bg-[#F2F2F2]",
+        }}
+      />
+      <div className="w-[100%] ss-container px-1 py-5 md:py-12 flex justify-center">
+        <div className="w-full md:w-[60%] lg:w-[55%] xl:w-[44%] h-full bg-[#F2F2F2] px-5  md:px-7 pt-10 rounded-3xl">
+          <div
+            className={`border  rounded-[20px] p-8 bg-white shadow-lg relative overflow-hidden`}
+          >
+            <div className=" absolute top-0 right-0 bg-[#D63348] p-4 rounded-es-xl cursor-pointer">
+              <FaEdit color="white" size={22} className="" />
             </div>
-        </>
-    )
-}
+            <div className="">
+              <span className="">
+                <img
+                  src={`https://bit.ly/dan-abramov`}
+                  alt=""
+                  className="h-28 md:h-40 rounded-full "
+                  style={{
+                    border: "4px solid #D63348",
+                  }}
+                />
+              </span>
+              <div className="mt-3">
+                <h3 className="popins font-semibold text-[20px]  md:text-[24px]">
+                  John Doe
+                </h3>
+                <div className=" my-2 popins text-16px md:text-[18px] text-[#777777]">
+                  examplejohn@gmail.com
+                </div>
+                <div className=" flex items-center gap-4 my-6 popins text-[16px] md:text-[18px]">
+                  <div className="bg-[#D63348] text-white rounded-full">
+                    <FaPhoneAlt className="  m-2" size={22} />
+                  </div>
+                  <span className="text-[#777777]">+234 9011039271</span>
+                </div>
+                <div className=" flex items-center gap-4 popins text-[16px] md:text-[18px] my-2 ">
+                  <div className="bg-[#D63348] text-white rounded-full">
+                    <ImLocation
+                      className="text-[18px] md:text-[20px] m-2"
+                      size={22}
+                    />
+                  </div>
+                  <span className="text-[#777777]">
+                    No 15 Test Street London, UK, SE1 1AB
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {data.map((item) => {
+            return <SmallCard key={item.name} data={item} />;
+          })}
+          <div className="text-center mb-8">
+            <button className="btn-red ">logout</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
